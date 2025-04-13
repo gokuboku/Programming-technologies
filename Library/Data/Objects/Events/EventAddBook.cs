bbb59744d3c5058545e1f3b7e1bbcb08d0cb7933
Robertas R. <robertas.radzvilas@ktu.edu>
2025-04-13 17:05:06 +0200

    Implemented Data layer

---
+++ Library/Data/Objects/Events/EventAddBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Data.Objects.Events
+{
+    internal class EventAddBook: Event
+    {
+        public EventAddBook(Guid bookId, DateTime date) : base("Book_added", date)
+        {
+            BookId = bookId;
+        }
+        public Guid BookId { get; private set; }
+    }
+}
---
+++ Library/Data/Objects/Events/EventRemoveBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Data.Objects.Events
+{
+    internal class EventRemoveBook: Event
+    {
+        public EventRemoveBook(Guid bookId, DateTime date) : base("Book_removed", date)
+        {
+            BookId = bookId;
+        }
+        public Guid BookId { get; private set; }
+    }
+}
