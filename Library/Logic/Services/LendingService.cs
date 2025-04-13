5c7c16153188eb8471fb341d0230a00933a14548
Deernode <99195911+Deernode@users.noreply.github.com>
2025-04-13 16:52:41 +0200

    implemented logic layer

---
+++ Library/Logic/Services/LendingService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Data;
+using Library.Data.Interfaces;
+using Library.Data.Objects;
+
+namespace Library.Logic.Services
+{
+    internal class LendingService
+    {
+        private IRepository repo;
+
+        public LendingService(IRepository repository)
+        {
+            this.repo = repository;
+        }
+
+        public void BorrowBook(Guid bookGuid, Guid userGuid) => repo.BorrowBook(bookGuid, userGuid);
+        public void ReturnBook(Guid bookGuid, Guid userGuid) => repo.ReturnBook(bookGuid, userGuid);
+
+        public void GiveFine(Guid userGuid, double amount) => repo.GiveFine(userGuid, amount);
+        public void PayFine(Guid userGuid, double amount) => repo.PayFine(userGuid, amount);
+    }
+}
---
+++ Library/Logic/Services/UserService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Data;
+using Library.Data.Interfaces;
+using Library.Data.Objects;
+
+namespace Library.Logic.Services
+{
+    internal class UserService
+    {
+        private IRepository repo;
+
+        public UserService(IRepository repository)
+        {
+            this.repo = repository;
+        }
+
+        public void AddUser(string name, string surname, string email) => repo.AddUser(name, surname, email);
+        public void RemoveUser(Guid guid) => repo.RemoveUser(guid);
+    }
+}
